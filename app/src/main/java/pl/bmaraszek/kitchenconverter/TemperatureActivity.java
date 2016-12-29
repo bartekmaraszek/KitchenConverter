63ab9f7a6b53db9db30a28da446ca281a61cbfe1
Bartek Maraszek <bm185154@ncr.com>
2016-12-29 17:11:05 +0000

    Add tempereture calculator

--- app/src/main/java/pl/bmaraszek/kitchenconverter/TemperatureActivity.java
+++ app/src/main/java/pl/bmaraszek/kitchenconverter/TemperatureActivity.java
@@ -2,12 +2,101 @@ package pl.bmaraszek.kitchenconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
 public class TemperatureActivity extends AppCompatActivity {
+
+    public enum Temperature {
+        C, F
+    }
+
+    private static Temperature editing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
+        addListeners();
+    }
+
+    private Integer celsiusToFahrenheit(Integer temperature) {
+        return temperature * 9 / 5 + 32;
+    }
+
+    private Integer fahrenheitToCelsius(Integer temperature) {
+        return (temperature - 32) * 5 / 9;
+    }
+
+    private Integer stringToInt(final String number) {
+        Integer result;
+        try {
+            result = Integer.valueOf(number.toString());
+        } catch(Exception e) {
+            result = 0;
+        }
+        return result;
+    }
+
+    private void addListeners() {
+        final EditText fahrenheitTemperature = (EditText) findViewById(R.id.fahrenheitEdit);
+        final EditText celsiusTemperature = (EditText) findViewById(R.id.celsiusEdit);
+
+        fahrenheitTemperature.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                editing = Temperature.F;
+                return false;
+            }
+        });
+
+        celsiusTemperature.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                editing = Temperature.C;
+                return false;
+            }
+        });
+
+        fahrenheitTemperature.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editing == Temperature.F && fahrenheitTemperature.getText().length() > 0) {
+                    final Editable text = fahrenheitTemperature.getText();
+                    int fTemp = stringToInt(text.toString());
+                    celsiusTemperature.setText(fahrenheitToCelsius(fTemp) + "");
+                }
+            }
+        });
+
+        celsiusTemperature.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editing == Temperature.C && celsiusTemperature.getText().length() > 0) {
+                    final Editable text = celsiusTemperature.getText();
+                    int cTemp = stringToInt(text.toString());
+                    fahrenheitTemperature.setText(celsiusToFahrenheit(cTemp) + "");
+                }
+            }
+        });
     }
 }
